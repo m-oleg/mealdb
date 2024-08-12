fb8361d96f9974063417cbe311a33a47066b9fbc
m-oleg <yalamerok@gmail.com>
2024-08-12 11:21:56 +0300

    Initial

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="flex flex-col p-8">
+		<input 
+			type="text" 
+			class="rounded border-2 border-gray-200 w-full" 
+			placeholder="Search for Meals"
+		>
+
+		<div class="flex gap-2 mt-2 justify-center">
+			<router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+				{{ letter }}
+			</router-link>
+		</div>
+
+	</div>
+</template>
+
+<script setup>
+
+const letters = 'ABCDEFGHIJKLMOPQRSTUVWXYZ'.split('')
+</script>
